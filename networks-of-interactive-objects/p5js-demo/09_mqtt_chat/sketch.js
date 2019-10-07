f1c9951404abcf18799c495c377b1a0ce69e7ee5
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2019-10-07 14:55:57 +0200

    fix credential chat

--- networks-of-interactive-objects/p5js-demo/09_mqtt_chat/sketch.js
+++ networks-of-interactive-objects/p5js-demo/09_mqtt_chat/sketch.js
@@ -1,69 +1,69 @@
-client = new Paho.MQTT.Client("broker.shiftr.io", Number(443), "controlpanel");
+client = new Paho.MQTT.Client( "broker.shiftr.io", Number( 443 ), "controlpanel" );
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
-var receivedMessage="";
+var receivedMessage = "";
 
 var message, address, button;
 
-var destinationAddress="/minorinteractive/studio/chat/1"
-var personalAddress="/minorinteractive/studio/chat/1"
+var destinationAddress = "/minorinteractive/studio/chat/1"
+var personalAddress = "/minorinteractive/studio/chat/1"
 
 function setup() {
-	createCanvas(400, 400);
+  createCanvas( 400, 400 );
 
-	message = createInput();
-  message.position(10, 65);
+  message = createInput();
+  message.position( 10, 65 );
 
-	address = createInput();
-	address.position(message.x + message.width+10, 65);
-	address.value("/minorinteractive/studio/chat/1");
+  address = createInput();
+  address.position( message.x + message.width + 10, 65 );
+  address.value( "/minorinteractive/studio/chat/1" );
 
-  button = createButton('submit');
-  button.position(message.x + message.width + address.width+20, 65);
-  button.mousePressed(sendToChat);
+  button = createButton( 'submit' );
+  button.position( message.x + message.width + address.width + 20, 65 );
+  button.mousePressed( sendToChat );
 
-  client.connect({
+  client.connect( {
     onSuccess: onConnect,
-    userName: "connected-object",
-    password: "c784e41dd3da48d4",
+    userName: "electro-forest",
+    password: "fe8708c4cd16348a",
     useSSL: true,
-  });
+  } );
 
-	textAlign(CENTER);
+  textAlign( CENTER );
 }
 
 function draw() {
-  background(220);
-	text(receivedMessage, width/2,height/2);
+  background( 220 );
+  text( receivedMessage, width / 2, height / 2 );
 }
 
-function sendToChat(){
-	var m=message.value();
-	destinationAddress=address.value();
-	sendMessage(destinationAddress,m);
+function sendToChat() {
+  var m = message.value();
+  destinationAddress = address.value();
+  sendMessage( destinationAddress, m );
 }
 
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
-  console.log("connected to mosquitto");
-  client.subscribe(personalAddress);
+  console.log( "connected to mosquitto" );
+  client.subscribe( personalAddress );
 }
 
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:" + responseObject.errorMessage);
+function onConnectionLost( responseObject ) {
+  if ( responseObject.errorCode !== 0 ) {
+    console.log( "onConnectionLost:" + responseObject.errorMessage );
   }
 }
 
-function onMessageArrived(message) {
-  console.log(message.destinationName + " -> " + message.payloadString);
+function onMessageArrived( message ) {
+  console.log( message.destinationName + " -> " + message.payloadString );
   receivedMessage = message.payloadString;
 }
 
-function sendMessage(topic, message) {
-  m = new Paho.MQTT.Message(message);
+function sendMessage( topic, message ) {
+  m = new Paho.MQTT.Message( message );
   m.destinationName = topic;
-	console.log("sending "+message+" to address:"+topic);
-  client.send(m);
+  console.log( "sending " + message + " to address:" + topic );
+  client.send( m );
 }
